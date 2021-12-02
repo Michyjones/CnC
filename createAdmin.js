9b9128614286c27e79c835836c773e692a6e7aaa
Michyjones <mykembuguah11@gmail.com>
2021-12-02 12:38:24 +0300

    Add register admin router and function

---
+++ createAdmin.js
@@ -0,0 +1,47 @@
+// const app = require('../app');
+const prompt = require('prompt');
+const Account = require('../CnC/model/account');
+
+
+
+const schema = {
+    properties: {
+        username: {
+            pattern:  /^[a-zA-Z\s\-]+$/,
+            message: 'Username must contain only letters, spaces, or dashes.',
+            required: true,
+            description: 'Enter username'
+        },
+        password: {
+            hidden: true,
+            required: true,
+            description: 'Enter password'
+        },
+        confirmPassword: {
+            hidden: true,
+            required: true,
+            description: 'Confirm password'
+        }
+    }
+};
+
+prompt.start();
+prompt.get(schema, function(err, result) {
+    if (result.password === result.confirmPassword) {
+        const newAccount = {
+            username: result.username,
+            isAdmin: true,
+            regDate: Date.now(),
+            lastLogin: Date.now()
+        };
+        Account.register(new Account(newAccount), result.password, function(err, account) {
+            if (err) {
+                console.log('Username already in use. Please try another username');
+            } else {
+                console.log('Admin account created');
+            }
+        });
+    } else {
+        console.log('Error - passwords do not match');
+    }
+});
